432984effae84ad001b7fcf11d1b1bfd8fa61ed4
Igor Costa <igor.bioinfo@gmail.com>
2015-11-24 19:17:44 -0200

    Add scripts to make circos coordinates from putative loci fasta and final AL sum file.

--- al_circos.py
+++ al_circos.py
@@ -1,55 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #al_circos.py
-#03/2014
+
+''' Return anonymous regions coordinates for circos.'''
 
 __author__ = 'Igor Rodrigues da Costa'
 __contact__ = 'igor.bioinfo@gmail.com'
 
-import os
-import argparse
-import pickle
+from sys import argv
 
-def argument_parser(hlp=False):
+def get_regions(al_file):
+    #Concatenate all contiguous putative loci from fasta and write in regions.txt file.
+    circos = []
+    chromo = '0'
+    start = '0'
+    end = '0'
+    with open(al_file, 'r') as a:
+        for l in a:
+            if l[0] == '>':
+                chromo_new = l.split(':')[0].split('|')[-1]
+                start_new = l.split(':')[1]
+                end_new = l.split(':')[2][:-1]
+                #Concatenate contiguous regions. 
+                if chromo == chromo_new and start_new == end:
+                    print l
+                    print circos.pop()
+                    end = end_new
+                else:
+                    start = start_new
+                    end = end_new
+                    chromo = chromo_new
+                circos.append('\t'.join(['hs'+chromo, start, end]))
+    with open('regions.txt', 'w') as regions_file:
+        for c in circos:
+            regions_file.write(c + '\n')
 
-    parser = argparse.ArgumentParser(description = 'Prints circos list', add_help = False,
-                                     argument_default = None, fromfile_prefix_chars = '@',
-                                     formatter_class = argparse.RawTextHelpFormatter)
-    default_out = os.getcwd() + '/'
-    parser.add_argument("-h", "--help", action = "help", help = "Show this help message and exit.")
-    parser.add_argument('-o', '--outpath', nargs = '?', type = str, default = default_out,\
-                        dest = 'outpath', help = 'Path where the circos AL files.')
-    parser.add_argument('-i', '--infile', nargs = '?', type = str, default = default_out,\
-                        dest = 'infile', help = 'File where the best_hit dictionary was saved.')
-    args = parser.parse_args().__dict__
-    return args
+def circos(al_coords):
+    #Write AL coordinates in al_circos.txt file.
+    with open(al_coords, 'r') as coords, open('al_circos.txt', 'w') as outfile:
+        for l in coords:
+            al_id = l.split()[0]
+            al_chromo = l.split()[1]
+            al_start = l.split()[2]
+            al_end = l.split()[3]
+            outfile.write('\t'.join(['hs'+al_chromo, al_start, al_end])+'\n')
 
-def main(args):
-    with open(args['infile'], 'r') as l:
-        locus, dict_hits = pickle.load(l)
-    als = [] #human als
-    for k in dict_hits:
-	if 'Homo' in k:
-            homo_key = k
-            with open(args['outpath']+k.split('/')[-1].replace('BLASTDB', '').split('_')[0]+'.list', 'w') as out:
-                for al in dict_hits[k]:
-                    als.append(al)
-                    chromo = (dict_hits[k][al][0])
-                    start = str(dict_hits[k][al][1])
-                    end = str(dict_hits[k][al][2])
-                    out.write('\t'.join(('hs' + chromo, start, end)) + '\n')
-    for k in dict_hits:
-        if not 'Homo' in k:
-            with open(args['outpath']+k.split('/')[-1].replace('BLASTDB', '').split('_')[0]+'.list', 'w') as out:
-                for al in dict_hits[k]:
-                    if al in als:
-                        chromo = (dict_hits[homo_key][al][0])
-                        start = str(dict_hits[homo_key][al][1])
-                        end = str(dict_hits[homo_key][al][2])
-                        out.write('\t'.join(('hs' + chromo, start, end)) + '\n')
-                    else:
-                        print al, 'was not found in ', k #should never happen!
+if len(argv) > 1:
+    circos(argv[1])
+    get_regions(argv[2])
+else:
+    print './al_circos.py al_coords pl_coords.fasta'
 
-if __name__ == '__main__':
-    args = argument_parser()
-    main(args)
