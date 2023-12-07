bb5399eaf2f8027c4328d82b99e0373df62ed603
Igor C <igor.bioinfo@gmail.com>
2023-12-07 12:19:16 -0300

    monkey patch Nexus to add indentation, fix header for interleaved all.nexus

--- al_align.py
+++ al_align.py
@@ -35,13 +35,187 @@ class NexusWriterInterleaved(NexusWriter):
         columns = alignment.get_alignment_length() 
         if columns == 0: 
             raise ValueError("Non-empty sequences are required") 
-        minimal_record = "#NEXUS\nbegin data; dimensions ntax=0 nchar=0; " \
+        minimal_record = "#NEXUS\n    begin data; dimensions ntax=0 nchar=0; " \
                          + "format datatype=dna; end;"  
         n = Nexus.Nexus(minimal_record) 
         for record in alignment: 
             n.add_sequence(record.id, str(record.seq)) 
         n.write_nexus_data(self.handle, interleave=True) 
 
+
+def monkey_write_nexus_data(
+    self,
+    filename=None,
+    matrix=None,
+    exclude=(),
+    delete=(),
+    blocksize=None,
+    interleave=False,
+    interleave_by_partition=False,
+    comment=None,
+    omit_NEXUS=False,
+    append_sets=True,
+    mrbayes=False,
+    codons_block=True,
+):
+    """Write a nexus file with data and sets block to a file or handle.
+
+    Character sets and partitions are appended by default, and are
+    adjusted according to excluded characters (i.e. character sets
+    still point to the same sites (not necessarily same positions),
+    without including the deleted characters.
+
+    - filename - Either a filename as a string (which will be opened,
+      written to and closed), or a handle object (which will
+      be written to but NOT closed).
+    - interleave_by_partition - Optional name of partition (string)
+    - omit_NEXUS - Boolean.  If true, the '#NEXUS' line normally at the
+      start of the file is omitted.
+
+    Returns the filename/handle used to write the data.
+    """
+    if not matrix:
+        matrix = self.matrix
+    if not matrix:
+        return
+    if not filename:
+        filename = self.filename
+    if [t for t in delete if not self._check_taxlabels(t)]:
+        raise NexusError(
+            "Unknown taxa: %s"
+            % ", ".join(set(delete).difference(set(self.taxlabels)))
+        )
+    if interleave_by_partition:
+        if interleave_by_partition not in self.charpartitions:
+            raise NexusError("Unknown partition: %r" % interleave_by_partition)
+        else:
+            partition = self.charpartitions[interleave_by_partition]
+            # we need to sort the partition names by starting position
+            # before we exclude characters
+            names = _sort_keys_by_values(partition)
+            newpartition = {}
+            for p in partition:
+                newpartition[p] = [c for c in partition[p] if c not in exclude]
+    # how many taxa and how many characters are left?
+    undelete = [
+        taxon for taxon in self.taxlabels if taxon in matrix and taxon not in delete
+    ]
+    cropped_matrix = _seqmatrix2strmatrix(
+        self.crop_matrix(matrix, exclude=exclude, delete=delete)
+    )
+    ntax_adjusted = len(undelete)
+    nchar_adjusted = len(cropped_matrix[undelete[0]])
+    if not undelete or (undelete and undelete[0] == ""):
+        return
+
+    with File.as_handle(filename, mode="w") as fh:
+        if not omit_NEXUS:
+            fh.write("#NEXUS\n")
+        if comment:
+            fh.write("[" + comment + "]\n")
+        fh.write("begin data;\n")
+        fh.write("    dimensions ntax=%d nchar=%d;\n" % (ntax_adjusted, nchar_adjusted))
+        fh.write("    format datatype=" + self.datatype)
+        if self.respectcase:
+            fh.write(" respectcase")
+        if self.missing:
+            fh.write(" missing=" + self.missing)
+        if self.gap:
+            fh.write(" gap=" + self.gap)
+        if self.matchchar:
+            fh.write(" matchchar=" + self.matchchar)
+        if self.labels:
+            fh.write(" labels=" + self.labels)
+        if self.equate:
+            fh.write(" equate=" + self.equate)
+        if interleave or interleave_by_partition:
+            fh.write(" interleave")
+        fh.write(";\n")
+        # if self.taxlabels:
+        #    fh.write('taxlabels '+' '.join(self.taxlabels)+';\n')
+        if self.charlabels:
+            newcharlabels = self._adjust_charlabels(exclude=exclude)
+            clkeys = sorted(newcharlabels)
+            fh.write(
+                "charlabels "
+                + ", ".join(
+                    "%s %s" % (k + 1, safename(newcharlabels[k])) for k in clkeys
+                )
+                + ";\n"
+            )
+        fh.write("matrix\n")
+        if not blocksize:
+            if interleave:
+                blocksize = 70
+            else:
+                blocksize = self.nchar
+        # delete deleted taxa and ecxclude excluded characters...
+        namelength = max(len(safename(t, mrbayes=mrbayes)) for t in undelete)
+        if interleave_by_partition:
+            # interleave by partitions, but adjust partitions with regard
+            # to excluded characters
+            seek = 0
+            for p in names:
+                fh.write("[%s: %s]\n" % (interleave_by_partition, p))
+                if len(newpartition[p]) > 0:
+                    for taxon in undelete:
+                        fh.write(
+                            safename(taxon, mrbayes=mrbayes).ljust(namelength + 1)
+                        )
+                        fh.write(
+                            cropped_matrix[taxon][
+                                seek : seek + len(newpartition[p])
+                            ]
+                            + "\n"
+                        )
+                    fh.write("\n")
+                else:
+                    fh.write("[empty]\n\n")
+                seek += len(newpartition[p])
+        elif interleave:
+            for seek in range(0, nchar_adjusted, blocksize):
+                for taxon in undelete:
+                    fh.write(safename(taxon, mrbayes=mrbayes).ljust(namelength + 1))
+                    fh.write(cropped_matrix[taxon][seek : seek + blocksize] + "\n")
+                fh.write("\n")
+        else:
+            for taxon in undelete:
+                if blocksize < nchar_adjusted:
+                    fh.write(safename(taxon, mrbayes=mrbayes) + "\n")
+                else:
+                    fh.write(safename(taxon, mrbayes=mrbayes).ljust(namelength + 1))
+                taxon_seq = cropped_matrix[taxon]
+                for seek in range(0, nchar_adjusted, blocksize):
+                    fh.write(taxon_seq[seek : seek + blocksize] + "\n")
+                del taxon_seq
+        fh.write(";\nend;\n")
+        if append_sets:
+            if codons_block:
+                fh.write(
+                    self.append_sets(
+                        exclude=exclude,
+                        delete=delete,
+                        mrbayes=mrbayes,
+                        include_codons=False,
+                    )
+                )
+                fh.write(
+                    self.append_sets(
+                        exclude=exclude,
+                        delete=delete,
+                        mrbayes=mrbayes,
+                        codons_only=True,
+                    )
+                )
+            else:
+                fh.write(
+                    self.append_sets(
+                        exclude=exclude, delete=delete, mrbayes=mrbayes
+                    )
+                )
+    return filename
+        
+Nexus.write_nexus_data = monkey_write_nexus_data
 _FormatToWriter['nexus'] = NexusWriterInterleaved
 
 def argument_parser(hlp = False):
@@ -129,7 +303,7 @@ def main(args):
         sizes, nexus_files = make_nexus(args['outpath'], aligned_files, args['align_size'], args['pick'], args['nogaps'], vprint)
         if args['pick']:
             assert len(nexus_files) <= args['pick']
-        join_nexus(args['outpath'], sizes, nexus_files)
+        join_nexus(args['outpath'], sizes, nexus_files, ntax=len(args['genomes']))
         join_fasta(args['outpath'], nexus_files)
         make_phylip(args['outpath'], nexus_files)
         with open(args['outpath'] + 'sample.log', 'w') as sample_file:
@@ -468,7 +642,7 @@ def join_nexus_by_chromo(path, aligned_files, min_align_size, nogaps, vprint):
         sizes, nexus_files = make_nexus(path, chromo_dict[chromo], min_align_size, None, nogaps, vprint)
         join_nexus(path, sizes, nexus_files, 'all.%s.nexus'%chromo)
 
-def join_nexus(path, sizes, nexus_files, outfile='all.nexus'):
+def join_nexus(path, sizes, nexus_files, outfile='all.nexus', ntax=0):
     soma_old = 0
     soma = 0
     seqs = ''
@@ -484,7 +658,7 @@ begin sets;
         append += 'charset "locus' + str(n + 1) + '" = ' + str(soma_old + 1) + '-' + str(soma) + ';\n'
         soma_old = soma
     append += 'end;'
-    start = '#nexus\nbegin data;\n    nchar=' + str(soma) + ';\n    format datatype=DNA interleave missing=? gap=-;\n    matrix\n'''
+    start = '#NEXUS\nbegin data;\n    dimensions ntax=' + str(ntax) + ' nchar=' + str(soma) + ';\n    format datatype=dna missing=? gap=- interleave;\nmatrix\n'''
     nexus_files.sort()
     with open(path + outfile, 'w') as nex_out:
         nex_out.write(start)
